1b0434c81362bbd0fd50a322b708d7a3bbc5e73e
Jeremy Myers <rockymtnskier@gmail.com>
2024-09-11 22:32:33 -0600

    fix role name

--- hcp_projects.tf
+++ hcp_projects.tf
@@ -6,7 +6,7 @@ resource "hcp_project" "new_project" {
 # Create a service principal for the project as admin
 #####################################################
 resource "hcp_service_principal" "project_spn" {
-  name   = "spn-${var.project_name}-secrets-app-manager"
+  name   = "vault-secrets-app-manager"
   parent = hcp_project.new_project.resource_name
 }
 
@@ -14,6 +14,6 @@ resource "hcp_service_principal" "project_spn" {
 resource "hcp_project_iam_binding" "secrets_app_manager" {
   project_id   = hcp_project.new_project.resource_id
   principal_id = hcp_service_principal.project_spn.resource_id
-  role         = "secrets/app manager"
+  role         = "roles/secrets.app manager"
 }
 
