f5cb34d1897ad544b649284a7aea55dc17c90785
Jeremy Myers <rockymtnskier@gmail.com>
2024-09-11 22:15:59 -0600

    try new iam role

--- hcp_projects.tf
+++ hcp_projects.tf
@@ -6,14 +6,14 @@ resource "hcp_project" "new_project" {
 # Create a service principal for the project as admin
 #####################################################
 resource "hcp_service_principal" "project_spn" {
-  name   = "spn-${var.project_name}"
+  name   = "spn-${var.project_name}-secrets-app-manager"
   parent = hcp_project.new_project.resource_name
 }
 
 
-resource "hcp_project_iam_binding" "project_owner" {
+resource "hcp_project_iam_binding" "secrets_app_manager" {
   project_id   = hcp_project.new_project.resource_id
   principal_id = hcp_service_principal.project_spn.resource_id
-  role         = "roles/admin"
+  role         = "secrets/app manager"
 }
 
