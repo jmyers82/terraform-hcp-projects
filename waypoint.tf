a4bbc7bd0533e8219450b46abaee750f3ac622be
jeremy myers <rockymtnskier@gmail.com>
2024-09-19 00:44:10 -0600

    fmt

--- waypoint.tf
+++ waypoint.tf
@@ -1,8 +1,8 @@
 resource "hcp_waypoint_template" "app_specific_template" {
-  name        = "${hcp_project.new_project.resource_name}-app-creator"
-  description = "Create Vault Secrets Apps in the ${hcp_project.new_project.name} project."
-  project_id  = hcp_project.new_project.resource_id
-  summary = "Create Vault Secrets Apps in the ${hcp_project.new_project.name} project."
+  name                            = "${hcp_project.new_project.resource_name}-app-creator"
+  description                     = "Create Vault Secrets Apps in the ${hcp_project.new_project.name} project."
+  project_id                      = hcp_project.new_project.resource_id
+  summary                         = "Create Vault Secrets Apps in the ${hcp_project.new_project.name} project."
   terraform_no_code_module_source = "hashiconf22/vault-secrets-apps"
-  terraform_project_id = tfe_project.app_specific_project.id
+  terraform_project_id            = tfe_project.app_specific_project.id
 }
