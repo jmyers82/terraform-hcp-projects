9ede5e73269ed8e693ddfe8fd14ad8219ba86a20
jeremy myers <rockymtnskier@gmail.com>
2024-09-26 22:28:55 -0600

    add wait

--- waypoint.tf
+++ waypoint.tf
@@ -1,7 +1,15 @@
+## Add in a wait to allow new project to be created
+resource "time_sleep" "wait_60_seconds" {
+  depends_on = [hcp_project.new_project]
+
+  create_duration = "60s"
+}
+
 resource "hcp_waypoint_tfc_config" "tfe_config" {
   token        = var.tfe_token_tfe
   tfc_org_name = var.tfe_organization
   project_id   = hcp_project.new_project.resource_id
+  depends_on = [ time_sleep.wait_60_seconds ]
 }
 
 resource "hcp_waypoint_template" "app_specific_template" {
